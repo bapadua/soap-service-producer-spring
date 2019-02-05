564b987256cb3edb5713dcbb9d09cdd4d88127b1
Bruno A. Padua <bapadua@stefanini.com>
2019-02-05 15:55:43 -0200

    commit inicial

---
+++ src/main/java/com/spring/soap/api/SpringSoapIoApplication.java
@@ -0,0 +1,14 @@
+package com.spring.soap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapIoApplication.class, args);
+	}
+
+}
+
